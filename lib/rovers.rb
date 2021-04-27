dc77c15bc2e16645312330017360942d14e79a8a
Devon Neill <dev@neillfam.co.za>
2021-04-27 16:36:06 +0200

    Refactor

--- lib/rovers.rb
+++ lib/rovers.rb
@@ -3,22 +3,16 @@
 # A class for handling the Platform45 mars rovers
 class Rovers
   def initialize(scanning_instructions)
-    instructions = scanning_instructions.split(/\n/)
-    @valid_instructions = instructions.count == 5
-
+    @instructions = validate_and_clean(scanning_instructions)
     return unless @valid_instructions
 
-    @grid_size = instructions[0].split(' ')
-    @rover_1_start_position = instructions[1].split(' ')
-    @rover_1_commands = instructions[2].strip.split('')
-    @rover_1_final_position = ''
-    @rover_2_start_position = instructions[3].split(' ')
-    @rover_2_commands = instructions[4].strip.split('')
-    @rover_2_final_position = ''
+    @grid = load_grid
+    @rovers = load_rovers
+    @final_positions = []
   end
 
   def scan_territory
-    return '********** ERROR: INVALID SCANNING INSTRUCTIONS provided **********' unless @valid_instructions
+    return '********** ERROR: INVALID SCANNING INSTRUCTIONS PROVIDED **********' unless @valid_instructions
 
     puts '********** BEGINNING SCAN **********'
     execute_scan
@@ -27,102 +21,144 @@ class Rovers
     final_positions
   end
 
+  private
+
+  def validate_and_clean(scanning_instructions)
+    @valid_instructions = (scanning_instructions.count("\n").positive? && scanning_instructions.count("\n").even?)
+    scanning_instructions.split(/\n/)
+  end
+
+  def load_grid
+    @instructions[0].split(' ')
+  end
+
+  def load_rovers
+    rovers = []
+    rover_count.times do |i|
+      rovers << new_rover(i)
+    end
+    rovers
+  end
+
+  def rover_count
+    (@instructions.count - 1) / 2
+  end
+
+  def new_rover(index)
+    {
+      start_position: @instructions[2 * index + 1].split(' '),
+      commands: @instructions[2 * index + 2].strip.split(''),
+      final_position: ''
+    }
+  end
+
   def execute_scan
-    update_rover_1_position
-    update_rover_2_position
+    @rovers.each_with_index do |rover, index|
+      @rovers[index] = update_position_for(rover, index)
+      @final_positions << rover[:final_position]
+    end
   end
 
-  def final_positions
-    "#{@rover_1_final_position} \n #{@rover_2_final_position}"
+  def update_position_for(rover, index)
+    x = rover[:start_position][0]
+    y = rover[:start_position][1]
+    start_position = [x, y]
+    start_direction = rover[:start_position][2]
+
+    puts "********** UPDATING ROVER #{index + 1} LOCATION **********"
+    puts "COMMAND LIST RECEIVED: #{rover[:commands]}"
+    puts "ROVER START LOCATION #{[*start_position, start_direction]}"
+
+    rover[:final_position] = execute_commands(rover[:commands], start_position, start_direction)
+    puts "********** ROVER #{index + 1} LOCATION UPDATED TO #{rover[:final_position]}"
+    rover
   end
 
-  def update_rover_1_position
-    x = @rover_1_start_position[0]
-    y = @rover_1_start_position[1]
-    position = [x, y]
-    direction = @rover_1_start_position[2]
-
-    puts '********** UPDATING ROVER LOCATION **********'
-    puts "COMMAND LIST RECEIVED: #{@rover_1_commands}"
-    puts "ROVER START LOCATION #{[*position, direction]}"
-
-    @rover_1_commands.each do |command|
-      if command == 'L'
+  def execute_commands(commands, position, direction)
+    commands.each do |command|
+      case command
+      when 'L'
         direction = turn_left(direction)
-        puts "TURNING LEFT TO FACE #{direction}"
-      elsif command == 'R'
+      when 'R'
         direction = turn_right(direction)
-        puts "TURNING RIGHT TO FACE #{direction}"
-      elsif command == 'M'
+      when 'M'
         position = move(position, direction)
-        puts "MOVING TO NEW POSITION #{position}"
       end
     end
-    @rover_1_final_position = [*position, direction].join(' ')
-    puts "********** ROVER LOCATION UPDATED TO #{@rover_1_final_position}"
-  end
-
-  def update_rover_2_position
-    x = @rover_2_start_position[0]
-    y = @rover_2_start_position[1]
-    position = [x, y]
-    direction = @rover_2_start_position[2]
-
-    puts '********** UPDATING ROVER LOCATION **********'
-    puts "COMMAND LIST RECEIVED: #{@rover_2_commands}"
-    puts "ROVER START LOCATION #{[*position, direction]}"
-
-    @rover_2_commands.each do |command|
-      if command == 'L'
-        direction = turn_left(direction)
-        puts "TURNING LEFT TO FACE #{direction}"
-      elsif command == 'R'
-        direction = turn_right(direction)
-        puts "TURNING RIGHT TO FACE #{direction}"
-      elsif command == 'M'
-        position = move(position, direction)
-        puts "MOVING TO NEW POSITION #{position}"
-      end
-    end
-    @rover_2_final_position = [*position, direction].join(' ')
-    puts "********** ROVER LOCATION UPDATED TO #{@rover_2_final_position}"
+    [*position, direction].join(' ')
   end
 
   def turn_left(direction)
-    if direction == 'N'
+    case direction
+    when 'N'
       direction = 'W'
-    elsif direction == 'E'
+    when 'E'
       direction = 'N'
-    elsif direction == 'S'
+    when 'S'
       direction = 'E'
-    elsif direction == 'W'
+    when 'W'
       direction = 'S'
     end
+    puts "TURNING LEFT TO FACE #{direction}"
     direction
   end
 
   def turn_right(direction)
-    if direction == 'N'
+    case direction
+    when 'N'
       direction = 'E'
-    elsif direction == 'E'
+    when 'E'
       direction = 'S'
-    elsif direction == 'S'
+    when 'S'
       direction = 'W'
-    elsif direction == 'W'
+    when 'W'
       direction = 'N'
     end
+    puts "TURNING RIGHT TO FACE #{direction}"
     direction
   end
 
   def move(position, direction)
-    max_x = @grid_size[0].to_i
-    max_y = @grid_size[1].to_i
     x = position[0].to_i
     y = position[1].to_i
-    y = direction == 'N' && y <= max_y ? (y + 1) : y
-    x = direction == 'E' && x <= max_x ? (x + 1) : x
-    y = direction == 'S' && y.positive? ? (y - 1) : y
-    x = direction == 'W' && x.positive? ? (x - 1) : x
+    new_position = update_position(x, y, direction)
+    puts "MOVING TO NEW POSITION #{new_position}"
+    new_position
+  end
+
+  def update_position(x, y, direction)
+    case direction
+    when 'N'
+      y = attempt_move_north(y)
+    when 'E'
+      x = attempt_move_east(x)
+    when 'S'
+      y = attempt_move_south(y)
+    when 'W'
+      x = attempt_move_west(x)
+    end
     [x.to_s, y.to_s]
+  end
+
+  def attempt_move_north(y)
+    max_y = @grid[1].to_i
+    y < max_y ? (y + 1) : y
+  end
+
+  def attempt_move_east(x)
+    max_x = @grid[0].to_i
+    x < max_x ? (x + 1) : x
+  end
+
+  def attempt_move_south(y)
+    y.positive? ? (y - 1) : y
+  end
+
+  def attempt_move_west(x)
+    x.positive? ? (x - 1) : x
+  end
+
+  def final_positions
+    @final_positions.join(" \n ")
   end
 end
--- spec/rovers_spec.rb
+++ spec/rovers_spec.rb
@@ -4,14 +4,21 @@ RSpec.describe Rovers do
   describe '#scan_territory' do
     context 'given a invalid input' do
       it 'returns an error message' do
-        expect(Rovers.new('invalid input').scan_territory).to eql '********** ERROR: INVALID SCANNING INSTRUCTIONS provided **********'
+        expect(Rovers.new('invalid input').scan_territory).to eql '********** ERROR: INVALID SCANNING INSTRUCTIONS PROVIDED **********'
       end
     end
 
-    context 'given valid scanning instructions' do
+    context 'given the default scanning instructions' do
       it 'returns final rover positions' do
         scanning_instructions = "5 5 \n 1 2 N \n LMLMLMLMM \n 3 3 E \n MMRMMRMRRM"
         expect(Rovers.new(scanning_instructions).scan_territory).to eql "1 3 N \n 5 1 E"
+      end
+    end
+
+    context 'given custom scanning instructions' do
+      it 'returns final rover positions' do
+        scanning_instructions = "6 6 \n 5 3 E \n MMRMM \n 2 4 E \n MRMMMRMLLM \n 0 2 N \n LMLMM"
+        expect(Rovers.new(scanning_instructions).scan_territory).to eql "6 1 S \n 3 1 E \n 0 0 S"
       end
     end
   end
